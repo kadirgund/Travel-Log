2a5045fd8d7b3bd46b4218fde3aaf9d81f9fb35c
Kadir Gundogdu <kadirgund@gmail.com>
2020-11-18 13:46:34 -0500

    minor change

--- client/src/API.js
+++ client/src/API.js
@@ -6,21 +6,21 @@ export async function listLogEntries() {
 }
 
 export async function createLogEntry(entry) {
-	const apiKey = entry.apiKey;
-	delete entry.apiKey;
+  const apiKey = entry.apiKey;
+  delete entry.apiKey;
   const response = await fetch(`${API_URL}/api/logs`, {
     method: 'POST',
     headers: {
-			'content-type': 'application/json',
-			'X-API-KEY': apiKey,
+      'content-type': 'application/json',
+      'X-API-KEY': apiKey,
     },
     body: JSON.stringify(entry),
   });
-	const json = await response.json();
-	if (response.ok) {
-		return json;
-	}
-	const error = new Error(json.message);
-	error.response = json;
-	throw error;
+  const json = await response.json();
+  if (response.ok) {
+    return json;
+  }
+  const error = new Error(json.message);
+  error.response = json;
+  throw error;
 }
