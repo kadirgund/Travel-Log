fc78157d8ff100e45cf2ef7f243aa1d00b57cc27
Kadir Gundogdu <kadirgund@gmail.com>
2020-11-18 14:05:59 -0500

    Change folder structure

---
+++ src/api/logs.js
@@ -0,0 +1,35 @@
+const { Router } = require('express');
+const LogEntry = require('../models/LogEntry');
+
+const { API_KEY } = process.env;
+
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    if (req.get('X-API-KEY') !== API_KEY) {
+      res.status(401);
+      throw new Error('Unauthorized');
+    }
+    const logEntry = new LogEntry(req.body);
+    const createdEntry = await logEntry.save();
+    res.json(createdEntry);
+  } catch (error) {
+    console.log(error.name);
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ src/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+// morgan logs metadata about incoming requests
+const morgan = require('morgan');
+// helmet prevents clients from seeing the framework used in the backend, increasing security
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+console.log(result.parsed);
+
+const middlewares = require('./middlewares');
+const logs = require('./api/logs');
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to database.');
+  })
+  .catch((err) => {
+    console.log('Not Connected to Database ERROR! ', err);
+  });
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.use('/api/logs', logs);
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}...`);
+});
